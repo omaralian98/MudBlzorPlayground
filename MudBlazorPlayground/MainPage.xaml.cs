30026bae612f58737b3cb52eaf3172ca1817b54d
omaralian98 <omaralian9870@gmail.com>
2024-08-11 00:23:04 +0300

    Inital

---
+++ MudBlazorPlayground/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MudBlazorPlayground
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MudBlazorPlayground/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MudBlazorPlayground
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
